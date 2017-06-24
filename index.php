30e933fb91b7fe26b4df1ca8ee873c460217365b
eduardobona <eduardobona@gmail.com>
2017-06-24 16:27:24 -0300

    enviando primeiros codigos para quebra-cabecas

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: UniCesumar
- * Date: 24/06/2017
- * Time: 16:08
- */
+
+define('TESTING', 1);
+
+$pecas_colunas = 8;
+$pecas_linhas = 6;
+$tamanho_peca = 100;
+
+include 'definir_dimensoes.php';
+include 'realizar_cortes.php';
+
+?>
