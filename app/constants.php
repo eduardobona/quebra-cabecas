24af4819d9845d38310492c6f5abcf7ca2f4c4b7
unknown <eduardobona@gmail.com>
2017-09-02 16:06:00 -0300

    informacao de ssl

--- app/constants.php
+++ app/constants.php
@@ -7,9 +7,11 @@ define('S3_SECRET_KEY', '');
 define('S3_BUCKET', '');
 define('S3_PATH', '');
 
-define('S3_ENDPOINT', 'http://' . S3_BUCKET . '.s3-website-us-east-1.amazonaws.com');
 define('S3_REGION', 'us-east-1');
+define('S3_ENDPOINT', 'http://' . S3_BUCKET . '.s3-website-'.S3_REGION.'.amazonaws.com');
 
+// utilizar na unicesumar apenas
+// https://curl.haxx.se/docs/caextract.html
 ini_set('curl.cainfo', "C:\tools\xampp\php\extras\ssl\cacert.pem");
 
 ?>
